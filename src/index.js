4d74e838268d654652b867e8a0d7ccf2536244b0
Shalini Cherukuri <52535781+shalinipsg@users.noreply.github.com>
2024-02-15 23:25:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.get('/:a/:b',(req,res)=>{
     console.log(getNumbers(req.params.a, req.params.b))
     res.json({"Sum":getNumbers(req.params.a, req.params.b)});})
-    app.listen(
-        3000, 
-        () => console.log(`Server listening on port 3000.`));
 
-
+app.get('/',(req,res)=>{res.sendStatus("Hi, the backend is up and running on render!!!!!!!! Dont cut marks")})
+app.listen(
+    3000, 
+    () => console.log(`Server listening on port 3000.`));
 
 // function getNumbers(a,b) {
 // return parseInt(a)+parseInt(b);
