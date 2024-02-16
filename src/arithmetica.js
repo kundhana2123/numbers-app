023c4a541b2c4aa4f5b7a95de8430be83bce802d
kundhana2123 <kundhana02@gmail.com>
2024-02-15 21:57:51 -0500

    subtraction function  added

--- src/arithmetica.js
+++ src/arithmetica.js
@@ -1,4 +1,24 @@
-function getNumbers(a,b) {
-    return parseInt(a)+parseInt(b);
-    }
-module.exports= { getNumbers }
+// function getNumbers(a,b) {
+//     return parseInt(a)+parseInt(b);
+//     }
+
+// function subtractNumbers(a,b) {
+//     console.log( (parseInt(a)-parseInt(b)))
+//         return  (parseInt(a)-parseInt(b));
+//         }
+// module.exports= { method:getNumbers() ,
+// othermethod: subtractNumbers() ,
+//  };
+function getNumbers(a, b) {
+    return parseInt(a) + parseInt(b);
+}
+
+function subtractNumbers(a, b) {
+    console.log(parseInt(a) - parseInt(b));
+    return parseInt(a) - parseInt(b);
+}
+
+module.exports = {
+    method: getNumbers,
+    othermethod: subtractNumbers
+};
--- src/test/arithmetica.test.js
+++ src/test/arithmetica.test.js
@@ -1,6 +1,13 @@
-const {getNumbers} = require('../arithmetica')
-test('2+5', ()=>{expect(getNumbers("2","5")).toBe(7)});
-test('5+5', ()=>{expect(getNumbers("5","5")).toBe(10)});
-test('2+9', ()=>{expect(getNumbers("2","9")).toBe(11)});
-test('2+20', ()=>{expect(getNumbers("2","20")).toBe(22)});
-test('2+15', ()=>{expect(getNumbers("2","15")).toBe(17)});
+const getNumbers = require('../arithmetica.js')
+// const {subtractNumbers} = require('../arithmetica')
+test('2+5', ()=>{expect(getNumbers.method("2","5")).toBe(7)});
+test('5+5', ()=>{expect(getNumbers.method("5","5")).toBe(10)});
+test('2+9', ()=>{expect(getNumbers.method("2","9")).toBe(11)});
+test('2+20', ()=>{expect(getNumbers.method("2","20")).toBe(22)});
+test('2+15', ()=>{expect(getNumbers.method("2","15")).toBe(17)});
+
+
+
+test('2-5', ()=>{expect(getNumbers.othermethod("2","5")).toBe(-3)});
+test('5-5', ()=>{expect(getNumbers.othermethod("5","5")).toBe(0)});
+test('2-9', ()=>{expect(getNumbers.othermethod("2","9")).toBe(-7)});
