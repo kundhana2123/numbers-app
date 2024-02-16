1369298fd1ee18dc22e443a30a4a8dca0739551e
Shalini Cherukuri <52535781+shalinipsg@users.noreply.github.com>
2024-02-15 22:51:34 -0500

    Update arithmetica.test.js

--- src/test/arithmetica.test.js
+++ src/test/arithmetica.test.js
@@ -1,33 +1,33 @@
-const arithmetica = require('../arithmetica.js');
+const { getNumbers, subtractNumbers } = require('./arithmetica.js');
 
 test('2+5', () => {
-    expect(arithmetica.method("2", "5")).toBe(7);
+    expect(getNumbers("2", "5")).toBe(7);
 });
 
 test('5+5', () => {
-    expect(arithmetica.method("5", "5")).toBe(10);
+    expect(getNumbers("5", "5")).toBe(10);
 });
 
 test('2+9', () => {
-    expect(arithmetica.method("2", "9")).toBe(11);
+    expect(getNumbers("2", "9")).toBe(11);
 });
 
 test('2+20', () => {
-    expect(arithmetica.method("2", "20")).toBe(22);
+    expect(getNumbers("2", "20")).toBe(22);
 });
 
 test('2+15', () => {
-    expect(arithmetica.method("2", "15")).toBe(17);
+    expect(getNumbers("2", "15")).toBe(17);
 });
 
 test('2-5', () => {
-    expect(arithmetica.othermethod("2", "5")).toBe(-3);
+    expect(subtractNumbers("2", "5")).toBe(-3);
 });
 
 test('5-5', () => {
-    expect(arithmetica.othermethod("5", "5")).toBe(0);
+    expect(subtractNumbers("5", "5")).toBe(0);
 });
 
 test('2-9', () => {
-    expect(arithmetica.othermethod("2", "9")).toBe(-7);
+    expect(subtractNumbers("2", "9")).toBe(-7);
 });
